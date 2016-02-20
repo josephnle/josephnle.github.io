0e8848654323367819bbde5a42ea9c11d5c2c2f9
Joseph Le <josephle@me.com>
2016-02-19 19:52:53 -0800

    Added images for portfolio page

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,32 +1,39 @@
 module.exports = [
     {
-      name: "Scouter",
-      type: "Startup",
-      status: "In-progress"
+        name: "Scouter",
+        type: "Startup",
+        image: "/images/projects/scouter/recruiter_mockup_cropped.png",
+        status: "In-progress"
     },
     {
-      name: "Campus Wayfinding",
-      type: "Everyday Design",
-      status: "In-progress"
+        name: "Campus Wayfinding",
+        type: "Everyday Design",
+        image: "/images/projects/wayfinding/wayfinding_funnel_cropped.png",
+        status: "In-progress"
     },
     {
-      name: "StudentHero",
-      type: "Startup"
+        name: "StudentHero",
+        image: "/images/projects/studenthero/homepage_cropped.png",
+        type: "Startup"
     },
     {
-      name: "SD Hacks",
-      type: "Web Development"
+        name: "SD Hacks",
+        type: "Web Development",
+        image: "/images/projects/sdhacks/home_cropped.png"
     },
     {
-      name: "Ice Blast",
-      type: "Web Design"
+        name: "Ice Blast",
+        type: "Web Design",
+        image: "/images/projects/iceblast/home_cropped.png"
     },
     {
-      name: "Basic Bites",
-      type: "Web Design"
+        name: "Basic Bites",
+        type: "Web Design",
+        image: "/images/projects/basicbites/home_cropped.png"
     },
     {
-      name: "Circle K",
-      type: "Web Design & Development"
+        name: "Circle K",
+        type: "Web Design & Development",
+        image: "/images/projects/basicbites/home_cropped.png"
     }
 ];

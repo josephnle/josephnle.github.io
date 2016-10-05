1e6453e588b1ec84a445b236533c1419bc87ef07
Joseph Le <josephle@me.com>
2016-10-05 01:14:12 -0700

    Added Workday

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,5 +1,11 @@
 module.exports = [
     {
+        name: "Workday",
+        type: "Internship",
+        image: "/images/projects/workday/workday-preview.png",
+        link: "/projects/workday.html"
+    },
+    {
         name: "Scouter",
         type: "Startup",
         image: "/images/projects/scouter/recruiter_mockup_cropped.png",
